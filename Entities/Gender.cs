d5bf548085994f2c6a097f72f10ec5ded68b4134
James Roberts <James@Robertscom.com>
2022-03-23 20:24:48 +1100

    Add Entities
    CASE-0

---
+++ Entities/Gender.cs
@@ -0,0 +1,24 @@
+﻿namespace CleanArchitecture.StaticEntity.Entities
+{
+
+    public class Gender
+    {
+
+        #region - - - - - - Constructors - - - - - -
+
+        public Gender(string name)
+            => this.Name = name;
+
+        #endregion Constructors
+
+        #region - - - - - - Properties - - - - - -
+
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        #endregion Properties
+
+    }
+
+}
---
+++ Entities/Person.cs
@@ -0,0 +1,27 @@
+﻿namespace CleanArchitecture.StaticEntity.Entities
+{
+
+    public class Person
+    {
+
+        #region - - - - - - Constructors - - - - - -
+
+        public Person(Gender gender, string name)
+        {
+            this.Gender = gender;
+            this.Name = name;
+        }
+
+        #endregion Constructors
+
+        #region - - - - - - Properties - - - - - -
+
+        public Gender Gender { get; set; }
+
+        public string Name { get; set; }
+
+        #endregion Properties
+
+    }
+
+}
